bc21a8a2861a7f72b858ffd653402e6cd2ae9ce1
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2019-11-23 12:41:04 -0300

    Insert players in scoreboard

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,6 +13,12 @@ public class MenuManager : MonoBehaviour
     [SerializeField]
     GameObject scoreBoard = null;
 
+    [SerializeField]
+    GameObject scoreText;
+
+    [SerializeField]
+    GameObject controlGroup;
+    
     public void Play() {
         string name = GameObject.Find("name").GetComponent<Text>().text;
         if(name == "") {
@@ -42,4 +48,30 @@ public class MenuManager : MonoBehaviour
         warnName.SetActive(false);
     }
 
+    void SetScoreBoard(Players players){
+        foreach (Transform child in controlGroup.transform) {
+            Destroy(child.gameObject);
+        }
+        Vector3 pos = new Vector3(0,0,0);
+        foreach(Player player in players.players){
+            GameObject textObject = Instantiate(scoreText, pos, Quaternion.identity);
+            textObject.transform.SetParent(controlGroup.transform);
+            Text text = textObject.GetComponent<Text>();
+            text.text = player.name + " : " + player.score;
+        }
+        scoreBoard.SetActive(true);
+    }
+
 }
+
+[Serializable]
+public class Player {
+    public string score;
+    public string name;
+}
+
+[Serializable]
+public class Players {
+    public Player[] players;
+}
+
